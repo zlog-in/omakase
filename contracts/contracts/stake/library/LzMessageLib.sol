830bb77f80dc94fb3761b1c6f74ff6fe18964022
Zion <user4cyber@gmail.com>
2025-07-05 12:40:15 +0200

    add lib to encode/decode lz message

---
+++ contracts/contracts/stake/library/LzMessageLib.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// =============================== Payload Types ===============================
+enum PayloadTypes {
+    LZ_RECEIVE,
+    STAKE_OMA,
+    UNSTAKE_OMA,
+    WITHDRAW_OMA,
+    CLAIM_REWARD,
+    SNED_REWARD
+}
+
+// =============================== LzMessage ===============================
+struct LzMessage {
+    uint8 payloadType;
+    bytes payload;
+}
+
+// =============================== Payloads ===============================
+struct StakePayload {
+    address staker;
+    uint256 amount;
+}
+
+struct UnstakePayload {
+    address staker;
+}
+
+struct WithdrawPayload {
+    address staker;
+}
+
+struct ClaimPayload {
+    address staker;
+}
+
+library LzMessageLib {
+    function encodeLzMessage(uint8 _payloadType, bytes memory _payload) internal pure returns (bytes memory) {
+        LzMessage memory lzMessage = LzMessage({payloadType: _payloadType, payload: _payload});
+        return abi.encode(lzMessage);
+    }
+
+    function decodeLzMessage(bytes memory _message) internal pure returns (LzMessage memory) {
+        return abi.decode(_message, (LzMessage));
+    }
+
+    function encodeStakePayload(address _staker, uint256 _amount) internal pure returns (bytes memory) {
+        return abi.encode(_staker, _amount);
+    }
+
+    function decodeStakePayload(bytes memory _payload) internal pure returns (StakePayload memory) {
+        (address staker, uint256 amount) = abi.decode(_payload, (address, uint256));
+        return StakePayload({staker: staker, amount: amount});
+    }
+
+    function encodeUnstakeMsg(address _staker) internal pure returns (bytes memory) {
+        return abi.encode(_staker);
+    }
+
+    function decodeUnstakePayload(bytes memory _payload) internal pure returns (UnstakePayload memory) {
+        (address staker) = abi.decode(_payload, (address));
+        return UnstakePayload({staker: staker});
+    }
+
+    function encodeWithdrawMsg(address _staker) internal pure returns (bytes memory) {
+        return abi.encode(_staker);
+    }
+
+    function decodeWithdrawPayload(bytes memory _payload) internal pure returns (WithdrawPayload memory) {
+        (address staker) = abi.decode(_payload, (address));
+        return WithdrawPayload({staker: staker});
+    }
+
+    function encodeClaimPayload(address _staker) internal pure returns (bytes memory) {
+        return abi.encode(_staker);
+    }
+
+    function decodeClaimPayload(bytes memory _payload) internal pure returns (ClaimPayload memory) {
+        (address staker) = abi.decode(_payload, (address));
+        return ClaimPayload({staker: staker});
+    }
+}
